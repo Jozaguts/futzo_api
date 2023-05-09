68ce68f8de1af8b4da6a85088c35ddfb750904d5
Sagit Gutierrez <jozaguts@gmail.com>
2023-05-09 09:42:18 -0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tenant;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+         \App\Models\User::factory()->create([
+             'name' => fake()->name(),
+             'email' => 'admin@sls.com',
+             'email_verified_at' => now(),
+             'password' => bcrypt('password'),
+             'remember_token' => Str::random(10),
+             'tenant_id' => Tenant::factory()->create(),
+         ]);
          \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
     }
 }
